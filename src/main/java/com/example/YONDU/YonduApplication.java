7cc4a2adff702421f0a09f3b82dec830157e4a2f
Jisub-space <kjs123@office365.kunsan.ac.kr>
2025-03-17 19:51:39 +0900

    init
    init

---
+++ src/main/java/com/example/YONDU/YonduApplication.java
@@ -0,0 +1,13 @@
+package com.example.YONDU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YonduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YonduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/YONDU/YonduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.YONDU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YonduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
